f42657b2aad0c7cc346a923aa73c52683c79bb6f
vincent19974 <45345593+vincent19974@users.noreply.github.com>
2020-11-24 07:19:03 +0800

    Update rec.sh

--- rec.sh
+++ rec.sh
@@ -1,10 +1,10 @@
-       conda create --name py38 python=3.8
-       conda init bash
-       conda activate py38
-       pip3 install pandas
-       pip3 install pyhive
-       pip3 install wheel
-       pip3 install thrift
-       sudo yum install python3-devel
-       sudo yum install gcc-c++ python-devel.x86_64 cyrus-sasl-devel.x86_64
-       pip3 install thrift-sasl
+conda create --name py38 python=3.8
+conda init bash
+conda activate py38
+pip3 install pandas
+pip3 install pyhive
+pip3 install wheel
+pip3 install thrift
+sudo yum install python3-devel
+sudo yum install gcc-c++ python-devel.x86_64 cyrus-sasl-devel.x86_64
+pip3 install thrift-sasl
