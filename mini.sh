22a587ff5e0375274479401b5f4546295c155254
vincent19974 <45345593+vincent19974@users.noreply.github.com>
2020-11-24 07:18:27 +0800

    Create mini.sh

---
+++ mini.sh
@@ -0,0 +1,4 @@
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+bash ~/miniconda.sh -b -p $HOME/miniconda
+conda init bash
+eval "$(/home/hadoop/miniconda/bin/conda shell.bash hook)"
